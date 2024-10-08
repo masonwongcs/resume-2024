34be7208f8e534ff291720fd69e52844c3293753
masonwongcs <masonwongcs@gmail.com>
2024-10-09 00:49:20 +0800

    feat: add stickers to skills

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,7 +2,179 @@ import styles from './Contact.module.scss';
 
 import cx from 'classnames';
 
+import { Sticker } from '@/components/Sticker';
+
 import QR from './qr.svg';
+
+const frontEndStickers = [
+  {
+    src: '/images/sticker/react.svg',
+    alt: 'React Sticker',
+    startX: '65vw',
+    startY: '10vh',
+    transformEndX: 300,
+    transformEndY: 0
+    // startX: '2vw',
+    // startY: '1vh',
+    // transformEndX: -300,
+    // transformEndY: -300
+  },
+  {
+    src: '/images/sticker/css.svg',
+    alt: 'CSS Sticker',
+    startX: '88vw',
+    startY: '5vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/html.svg',
+    alt: 'HTML Sticker',
+    startX: '78vw',
+    startY: '50vh',
+    transformEndX: 300,
+    transformEndY: 100
+  },
+  {
+    src: '/images/sticker/javascript.svg',
+    alt: 'JavaScript Sticker',
+    startX: '65vw',
+    startY: '70vh',
+    transformEndX: -300,
+    transformEndY: 300
+  },
+  {
+    src: '/images/sticker/typescript.svg',
+    alt: 'TypeScript Sticker',
+    startX: '85vw',
+    startY: '75vh',
+    transformEndX: 300,
+    transformEndY: 300
+  },
+  {
+    src: '/images/sticker/nextjs.svg',
+    alt: 'Next.js Sticker',
+    startX: '75vw',
+    startY: '-5vh',
+    transformEndX: -200,
+    transformEndY: -400
+  },
+  {
+    src: '/images/sticker/figma.svg',
+    alt: 'Figma Sticker',
+    startX: '75vw',
+    startY: '30vh',
+    transformEndX: 200,
+    transformEndY: -300
+  }
+  // { src: '/images/sticker/nextjs.svg', alt: 'React Sticker' },
+  // { src: '/images/sticker/figma.svg', alt: 'React Sticker' }
+];
+
+const backendStickers = [
+  {
+    src: '/images/sticker/nodejs.svg',
+    alt: 'Node.js Sticker',
+    startX: '70vw',
+    startY: '5vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/graphql.svg',
+    alt: 'GraphQL Sticker',
+    startX: '80vw',
+    startY: '15vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/bunjs.svg',
+    alt: 'Bun.js Sticker',
+    startX: '75vw',
+    startY: '75vh',
+    transformEndX: 200,
+    transformEndY: 300
+  },
+  {
+    src: '/images/sticker/postgresql.svg',
+    alt: 'PostgresQL Sticker',
+    startX: '80vw',
+    startY: '50vh',
+    transformEndX: 200,
+    transformEndY: -200
+  }
+];
+
+const devopsSticker = [
+  {
+    src: '/images/sticker/aws.svg',
+    alt: 'AWS Sticker',
+    startX: '70vw',
+    startY: '5vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/gcloud.svg',
+    alt: 'Google Cloud Sticker',
+    startX: '80vw',
+    startY: '25vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/git.svg',
+    alt: 'Git Sticker',
+    startX: '75vw',
+    startY: '75vh',
+    transformEndX: 200,
+    transformEndY: 300
+  },
+  {
+    src: '/images/sticker/docker.svg',
+    alt: 'Docker Sticker',
+    startX: '80vw',
+    startY: '50vh',
+    transformEndX: 200,
+    transformEndY: -200
+  }
+];
+
+const softSkillSticker = [
+  {
+    src: '/images/sticker/atlassian.svg',
+    alt: 'Atlassian Sticker',
+    startX: '70vw',
+    startY: '5vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/miro.svg',
+    alt: 'Miro Sticker',
+    startX: '80vw',
+    startY: '25vh',
+    transformEndX: 300,
+    transformEndY: 0
+  },
+  {
+    src: '/images/sticker/jira.svg',
+    alt: 'Jira Sticker',
+    startX: '75vw',
+    startY: '75vh',
+    transformEndX: 200,
+    transformEndY: 300
+  },
+  {
+    src: '/images/sticker/slack.svg',
+    alt: 'Slack Sticker',
+    startX: '80vw',
+    startY: '50vh',
+    transformEndX: 200,
+    transformEndY: -200
+  }
+];
 
 const Group = () => {
   return (
@@ -35,6 +207,19 @@ const Contact = () => {
                 HTML, CSS, SCSS, JavaScript, Next.js, React.js, Three.js, PIXI.js, TypeScript, Responsive Design, State
                 Management
               </p>
+              <div className={styles.stickerWrapper}>
+                {frontEndStickers.map(({ src, alt, startX, startY, transformEndX, transformEndY }, index) => (
+                  <Sticker
+                    key={src}
+                    src={src}
+                    alt={alt}
+                    startX={startX}
+                    startY={startY}
+                    transformEndX={transformEndX}
+                    transformEndY={transformEndY}
+                  />
+                ))}
+              </div>
             </div>
 
             <div className={styles.innerGridItem}>
@@ -42,6 +227,19 @@ const Contact = () => {
               <p className={styles.description}>
                 Node.js, Express.js, Bun.js, Deno.js, RESTful APIs, GraphQL, SQL, PostgreSQL
               </p>
+              <div className={styles.stickerWrapper}>
+                {backendStickers.map(({ src, alt, startX, startY, transformEndX, transformEndY }, index) => (
+                  <Sticker
+                    key={src}
+                    src={src}
+                    alt={alt}
+                    startX={startX}
+                    startY={startY}
+                    transformEndX={transformEndX}
+                    transformEndY={transformEndY}
+                  />
+                ))}
+              </div>
             </div>
 
             <div className={styles.innerGridItem}>
@@ -49,6 +247,19 @@ const Contact = () => {
               <p className={styles.description}>
                 CI/CD (Jenkins, GitHub Actions), Docker, Kubernetes, Cloud Platform, AWS, Google Cloud Platform
               </p>
+              <div className={styles.stickerWrapper}>
+                {devopsSticker.map(({ src, alt, startX, startY, transformEndX, transformEndY }, index) => (
+                  <Sticker
+                    key={src}
+                    src={src}
+                    alt={alt}
+                    startX={startX}
+                    startY={startY}
+                    transformEndX={transformEndX}
+                    transformEndY={transformEndY}
+                  />
+                ))}
+              </div>
             </div>
 
             <div className={styles.innerGridItem}>
@@ -57,6 +268,19 @@ const Contact = () => {
                 Team Collaboration, Agile Methodologies, Scrum, Problem-solving, Mentorship and Leadership, Project
                 Management
               </p>
+              <div className={styles.stickerWrapper}>
+                {softSkillSticker.map(({ src, alt, startX, startY, transformEndX, transformEndY }, index) => (
+                  <Sticker
+                    key={src}
+                    src={src}
+                    alt={alt}
+                    startX={startX}
+                    startY={startY}
+                    transformEndX={transformEndX}
+                    transformEndY={transformEndY}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
---
+++ src/components/Sticker/Sticker.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import styles from './Sticker.module.scss';
+
+import React, { FC, useMemo } from 'react';
+
+import cx from 'classnames';
+
+interface StickerProps {
+  src: string;
+  alt?: string;
+  startX: string;
+  startY: string;
+  transformEndX: number;
+  transformEndY: number;
+}
+
+const Sticker: FC<StickerProps> = ({ src, alt = 'Sticker', startX, startY, transformEndX, transformEndY }) => {
+  const { rotate, scale, duration, delay } = useMemo(
+    () => ({
+      rotate: Math.random() * 40 - 20,
+      scale: Math.random() * 0.2 + 0.85, // Random scale between 0.85 and 1.05
+      duration: Math.random() * 0.5 + 0.5, // Random duration between 0.5s and 1s
+      delay: Math.random() * 0.5 // Random delay between 0s and 0.5s
+    }),
+    []
+  );
+
+  return (
+    <div
+      className={cx(styles.sticker, 'sticker')}
+      style={
+        {
+          position: 'fixed',
+          left: startX,
+          top: startY,
+          transform: `translate(${transformEndX}px, ${transformEndY}px)`,
+          // transitionDuration: `${duration}s`,
+          // transitionDelay: `${delay}s`,
+          '--rotate': `${rotate}deg`,
+          '--scale': scale,
+          '--duration': `${duration}s`,
+          '--delay': `${delay}s`
+        } as React.CSSProperties
+      }
+    >
+      <img
+        src={src}
+        alt={alt}
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'contain'
+        }}
+      />
+    </div>
+  );
+};
+
+export { Sticker };
