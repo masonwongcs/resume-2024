9378c8f073de9e0ccb86b8161780d80d00612512
masonwongcs <masonwongcs@gmail.com>
2025-01-12 01:12:25 +0800

    feat: add loading animation

--- src/components/DisplacementOrb/DisplacementOrb.tsx
+++ src/components/DisplacementOrb/DisplacementOrb.tsx
@@ -5,12 +5,13 @@ import '@14islands/r3f-scroll-rig/css';
 import { RefObject, Suspense, memo, useRef, useState } from 'react';
 
 import { GlobalCanvas, SmoothScrollbar } from '@14islands/r3f-scroll-rig';
-import { Environment, Loader } from '@react-three/drei';
+import { Environment, Html, Loader } from '@react-three/drei';
 
 import { Blob } from '@/components/Background';
 import { Lens } from '@/components/DisplacementOrb/Lens';
 import { Headline } from '@/components/DisplacementOrb/Text';
 import { WebGLBackground } from '@/components/DisplacementOrb/WebGLBackground';
+import { CircularLoading, Loading } from '@/components/Loading';
 
 interface DisplacementOrbProps {
   children: React.ReactNode;
@@ -38,7 +39,13 @@ const DisplacementOrb = ({ children, showCursor, showBackground = true }: Displa
         {(globalChildren) => (
           <Lens hovered={hovered}>
             {showBackground && <WebGLBackground hovered={hovered} />}
-            <Suspense fallback="">
+            <Suspense
+              fallback={
+                <Html center>
+                  <CircularLoading />
+                </Html>
+              }
+            >
               <Environment files="env/empty_warehouse_01_1k.hdr" />
               {globalChildren}
             </Suspense>
