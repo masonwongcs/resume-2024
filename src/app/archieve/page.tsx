ef8a00e45ec1f5cbd5fdf16a4d7b570f73e8148f
masonwongcs <masonwongcs@gmail.com>
2025-12-10 19:58:29 +0800

    fix: build error

--- src/app/archieve/page.tsx
+++ src/app/archieve/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.scss';
+import styles from '../page.module.scss';
 
 import { DisplacementOrb } from '@/components/DisplacementOrb';
 import { Hero } from '@/components/Hero';
