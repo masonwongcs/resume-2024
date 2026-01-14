33766ef93ae61d9775de86282e75d3c51ca4fb28
masonwongcs <masonwongcs@gmail.com>
2026-01-14 10:11:14 +0800

    feat: bug fix for header width

--- src/components/GlassSurface/GlassSurface.tsx
+++ src/components/GlassSurface/GlassSurface.tsx
@@ -45,7 +45,7 @@ export interface GlassSurfaceProps {
 
 const GlassSurface: React.FC<GlassSurfaceProps> = ({
   children,
-  width = 200,
+  width = 500,
   height = 80,
   borderRadius = 20,
   borderWidth = 0.07,
